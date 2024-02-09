05c94930c2db12b89fa24d4e066e6be45e89f526
Mario Pedisic <mario.pedisic@gmail.com>
2024-02-09 11:30:42 +0100

    add tailwindcss and database read capability

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em=$em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ['Avengers: Endgame', 'Inception', 'Loki', 'Black Widow'];
+        //findAll,find,count,findBy, findByOne
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));
+        $repostitory = $this->em->getRepository(Movie::class);
+        $movies = $repostitory->findAll();
+        return $this->render('index.html.twig');
 
         /* return $this->json([
             'message' => 'You selected the movie ' . $name,
